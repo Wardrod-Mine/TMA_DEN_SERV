9471509b79ace18ec497d8f63707a466ade13690
Wardrod-Mine <wardrodmine@gmail.com>
2025-08-13 21:58:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 
-// Разрешаем фронту обращаться к бэку (замени на свой GitHub Pages URL)
-const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || "https://USERNAME.github.io";
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || "*";
 app.use(cors({ origin: ALLOWED_ORIGIN }));
 
 app.get("/", (_, res) => res.send("TMA backend is running"));
-
 app.post("/web-data", (req, res) => {
   console.log("web-data:", req.body);
-  // TODO: сохранить/обработать
   res.json({ ok: true });
 });
 
